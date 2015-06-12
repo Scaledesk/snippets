d6bf4464ea880d1702af2fb002194d28ad4ef560
tusharcoder <tamyworld@gmail.com>
2015-06-12 14:41:09 +0530

    Applying Md5 encryption in user and admin scope

--- application/admin/models/mo_admin_manage.php
+++ application/admin/models/mo_admin_manage.php
@@ -6,11 +6,12 @@ class Mo_Admin_Manage extends CI_Model {
         {
                 // Call the CI_Model constructor
                 parent::__construct();
+                $this->load->helper('security');
         }
 	
 	public function validate() {
 		$username = $this->input->post('username');
-		$password = $this->input->post('password');
+		$password = do_hash($this->input->post('password'),'md5');
 		$query = $this->db->get_where('admin', array('email' => $username, 'password' => $password));
 		if($query->num_rows() > 0)
 		{
@@ -26,14 +27,14 @@ class Mo_Admin_Manage extends CI_Model {
 	}
 	public function update_password() {
 		$username = $this->session->userdata('admin_email');
-		$current_password = $this->input->post('current_password');
+		$current_password = do_hash($this->input->post('current_password'),'md5');
 		$query = $this->db->get_where('admin', array('email' => $username, 'password' => $current_password));
 		
 		
 		if($query->num_rows() > 0)
 		{
-			$this->db->where(array('email'=>$username,-'password'=>$current_password));
-			$this->db->update('admin', array('password' => $this->input->post('new_password')));
+			$this->db->where(array('email'=>$username,'password'=>$current_password));
+			$this->db->update('admin', array('password' => do_hash($this->input->post('new_password'),'md5')));
 			if($this->db->affected_rows()==1){
 			return true;
 			}
--- application/models/mo_user.php
+++ application/models/mo_user.php
@@ -7,17 +7,19 @@ class Mo_User extends CI_Model
 	function __construct()
 	{
 		$this->load->database();
+		$this->load->helper('security');
 	}
 	public function register()
 	{
 		#code to register
-		if($this->db->insert('user',array('fname'=>$this->input->post('fname'),'lname'=>$this->input->post('lname'),'email'=>$this->input->post('email'),'pwd'=>$this->input->post('pwd')))){
+		if($this->db->insert('user',array('fname'=>$this->input->post('fname'),'lname'=>$this->input->post('lname'),'email'=>$this->input->post('email'),'pwd'=>do_hash($this->input->post('pwd'),'md5'))))
+		{
 			return true;
 		}return false;
 	}
 	public function login()
 	{
-		$q=$this->db->get_where('user',array('email'=>$email=$this->input->post('email'),'pwd'=>$this->input->post('pwd')));
+		$q=$this->db->get_where('user',array('email'=>$email=$this->input->post('email'),'pwd'=>do_hash($this->input->post('pwd'),'md5')));
 		if($q->num_rows()==1)
 		{
 				$this->session->set_userdata('logged_user',$q->row()->id);
@@ -26,8 +28,8 @@ class Mo_User extends CI_Model
 		}return false;
 	}
 	public function ch_pwd()
-	{if($this->db->get_where('user', array('id' => $this->session->userdata('logged_user'), 'pwd' => $cur_pwd =$this->input->post('cur_pwd')))->num_rows()==1)
-		{if($this->db->where(array('email'=>$this->input->post('email'),'pwd'=>$cur_pwd))->update('user', array('pwd' => $this->input->post('new_pwd'))))
+	{if($this->db->get_where('user', array('id' => $this->session->userdata('logged_user'), 'pwd' => $cur_pwd =do_hash($this->input->post('cur_pwd'),'md5')))->num_rows()==1)
+		{if($this->db->where(array('email'=>$this->input->post('email'),'pwd'=>$cur_pwd))->update('user', array('pwd' => do_hash($this->input->post('new_pwd'),'md5'))))
 			{
 				if($this->db->affected_rows()==1){
 				return true;
